b7b05b42f7f953c76341b129c4d171ccf10d70b2
maksat sarsenbaev <88890025m@gmail.com>
2023-11-03 00:37:59 +0600

    added video

--- app/src/main/java/com/example/labaeffective/MainActivity.kt
+++ app/src/main/java/com/example/labaeffective/MainActivity.kt
@@ -45,7 +45,10 @@ class MainActivity : ComponentActivity() {
                     .fillMaxSize()
             ) {
                 dotaPrew()
+//                dotaText()
                 description()
+                video()
+
             }
             Column(
                 modifier = Modifier.fillMaxHeight(0.45f),
@@ -59,6 +62,7 @@ class MainActivity : ComponentActivity() {
 }
 
 /** Картинка вверху экрана **/
+@Preview
 @Composable
 fun dotaPrew(){
     Box(
@@ -76,6 +80,7 @@ fun dotaPrew(){
 }
 
 /** Лого доты **/
+@Preview
 @Composable
 private fun dotaLogo() {
     Card(
@@ -86,28 +91,60 @@ private fun dotaLogo() {
         Box(
             modifier = Modifier
                 .background(colorResource(R.color.backColor))
-                .size(width = 88.dp, height = 95.dp),
+                .size(width = 88.dp, height = 94.5.dp),
             contentAlignment = Alignment.Center
         ) {
             Image(
                 painter = painterResource(id = R.drawable.logo_dota_),
                 contentDescription = "image",
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.size(90.dp)
+                modifier = Modifier.fillMaxSize()
             )
         }
     }
 }
+@Preview
+@Composable
+private fun dotaText() {
+    Card(
+        modifier = Modifier
+            .size(width = 120.dp, height = 80.dp)
+            .padding(22.dp, 50.dp, 100.dp)
+    ){
+        Box(
+            modifier = Modifier
+                .background(colorResource(R.color.backColor))
+                .size(width = 124.dp, height = 200.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "DoTA 2",
+                fontSize = 20.sp,
+                lineHeight = 26.sp,
+                fontWeight = FontWeight(700),
+                color = Color(0xB2EEF2FB),
+            )
+//                        color: #FFF;
+//            font-family: Sk-Modernist;
+//            font-size: 20px;
+//            font-style: normal;
+//            font-weight: 700;
+//            line-height: 26px; /* 130% */
+//            letter-spacing: 0.5px;)
+        }
+    }
+}
 
-
+@Preview
 @Composable
 private fun description() {
     Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 21.dp),
+        horizontalArrangement = Arrangement.Start
     ) {
         Box(
-            modifier = Modifier.size(height = 190.dp, width = 327.dp),
+            modifier = Modifier.size(height = 150.dp, width = 327.dp),
             contentAlignment = Alignment.BottomCenter
         ){
             Text(
@@ -122,4 +159,86 @@ private fun description() {
             )
         }
     }
+}
+@Composable
+private fun video() {
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(start=24.dp,top=18.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Box(
+            modifier = Modifier.size(width = 240.dp, height = 135.dp).padding(end = 15.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.video1),
+                contentDescription = "screen1",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.size(width=240.dp, height=135.dp)
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.ellipse),
+                contentDescription = "ellipse",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.size(45.dp)
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.arrow),
+                contentDescription = "arrow",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.size(24.dp)
+            )
+        }
+
+        Column(){
+            Image(
+                painter = painterResource(id = R.drawable.video2),
+                contentDescription = "screen2",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(width=240.dp, height=135.dp)
+            )
+        }
+
+    }
+}
+
+//@Preview
+//@Composable
+//private fun video(){
+//    Row(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(start = 24.dp, top = 18.dp, end = 0.dp),
+//
+//        ){
+//        Row(modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(start = 0.dp, top = 0.dp, end = 5.dp),
+//        ){
+//            Image(painter = painterResource(id = R.drawable.video1),
+//                contentDescription = "image",
+//                contentScale = ContentScale.Crop,
+//                modifier = Modifier.size(height = 135.dp, width = 240.dp) )
+//
+//        }
+//        Row(modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(start = 0.dp, top = 0.dp, end = 0.dp),
+//     ){
+//            Image(painter = painterResource(id = R.drawable.video2),
+//                contentDescription = "image",
+//                contentScale = ContentScale.Crop,
+//                modifier = Modifier.size(height = 138.dp, width = 90.dp))
+//        }
+//
+//
+//
+//    }
+//}
+@Preview
+@Composable
+private fun review(){
+    Row(){}
 }
